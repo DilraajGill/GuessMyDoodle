e75ad3e634a8611ec9809122f4c0e10a5c6b2e34
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-10-17 14:52:06 +0100

    Create model and schema for Mongoose data

--- auth.js
+++ auth.js
@@ -7,5 +7,12 @@ import { Strategy } from "passport-local";
 const router = express.Router()
 // Connect to MongoDB server
 mongoose.connect("mongodb://localhost:27017/project");
+const userSchema = mongoose.Schema({
+    email: String,
+    password: String
+});
+userSchema.plugin(passportLocalMongoose);
+const User = mongoose.Model("User", userSchema);
+
 
 export default router;
