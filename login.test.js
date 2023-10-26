ef616771a720c354b71e620827f6baf1585c188d
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-10-26 10:25:03 +0100

    Create login test file

---
+++ login.test.js
@@ -0,0 +1,10 @@
+import supertest from "supertest";
+import app from "./server";
+
+const request = supertest(app);
+
+describe("Login Tests", () => {
+    test("login with user", async () => {
+        
+    })
+})
