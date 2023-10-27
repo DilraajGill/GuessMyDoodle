b91498c5559c45e8458a93398419d7d6cd95c085
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-10-27 13:29:54 +0100

    Customise package.json to allow front-end testing

---
+++ front-end/src/Canvas.test.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Canvas from "./Canvas";
+import {render} from "@testing-library/react";
+
+describe("Canvas Tests", () => {
+    test("rendering canvas", () => {
+        console.log("Hello");
+    })
+})
