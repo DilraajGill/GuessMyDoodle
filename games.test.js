0620b1930eddaa7e6c63e80d30c1c08b82bca519
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-10-30 20:30:34 +0000

    Install socket.io for managing real-time connections

--- games.test.js
+++ games.test.js
@@ -1,3 +1,10 @@
 import supertest from "supertest";
 import app from "./server";
+import { io } from "socket.io-client";
 
+const request = supertest(app);
+describe("game tests", () => {
+    test("connection", async () => {
+        
+    })
+})
