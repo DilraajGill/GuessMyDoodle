38076c0e68c8ee6517d551d6b30f417f23937c81
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-10-31 16:59:43 +0000

    Create test for ensuring connection

--- games.test.js
+++ games.test.js
@@ -3,8 +3,13 @@ import app from "./server";
 import { io } from "socket.io-client";
 
 const request = supertest(app);
+
+let client;
+beforeAll(() => {
+    client = io.connect("http://localhost:3001");
+})
 describe("game tests", () => {
     test("connection", async () => {
-        
+        expect(client.connected).toBe(true);
     })
 })
