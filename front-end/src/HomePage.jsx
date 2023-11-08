db4d1264d6e235e4fd916643285da4a20ec80469
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-11-08 15:12:12 +0000

    Imported context into HomePage

--- front-end/src/HomePage.jsx
+++ front-end/src/HomePage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import axios from "axios";
+import { authContext } from "./App";
+import { useNavigate } from "react-router-dom";
 
 function HomePage(){
+    const [signedIn, setSignedIn] = React.useContext(authContext);
+    const navigation = useNavigate();
     return(
         <h1>Home Page</h1>
     )
