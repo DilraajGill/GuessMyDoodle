abc8f06a1d9efd0004395927ea266ededc4d8725
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-11-08 15:10:30 +0000

    Add context and states to manage and store login authentication status

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -5,16 +5,22 @@ import HomePage from "./HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Canvas from "./Canvas";
 
+export const authContext = React.createContext();
+
 function App() {
+  const [signedIn, setSignedIn] = React.useState({auth: false});
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="canvas" element={<Canvas />} />
-      </Routes>
-    </BrowserRouter>
+    <authContext.Provider value={[signedIn, setSignedIn]}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="canvas" element={<Canvas />} />
+        </Routes>
+      </BrowserRouter>
+    </authContext.Provider>
+    
   );
 }
 
