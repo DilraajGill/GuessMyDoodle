1bcf928ace07eafeb3d3c428954eecb424beccc3
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-11-09 18:12:01 +0000

    Add code to pass first TDD test

--- front-end/src/checkAuthentication.jsx
+++ front-end/src/checkAuthentication.jsx
@@ -1,4 +1,8 @@
-function checkAuthentication({axios, navigate}){
+async function checkAuthentication({axios, navigate}){
+    const response = await axios.get("/auth/check-auth");
 
+    if (!response.data.auth){
+        navigate("/login");
+    }
 }
 export default checkAuthentication;
