ab73c8d814401a42668940710ed10ea442cf73c6
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-11-09 19:10:03 +0000

    Defined tests for back-end checking authenticatin

--- check-auth.test.js
+++ check-auth.test.js
@@ -4,10 +4,19 @@ const request = supertest(app);
 
 describe("check-authentication", () => {
     test("authentication when signed in", async () => {
-
+        const testUser = {
+            username: "Test",
+            password: "testing123"
+        }
+        const response = await request.post("/auth/login")
+                                        .send(testUser)
+                                        .set("Content-Type", "application/json");
+        const testAuth = await request.get("/auth/check-auth");
+        expect(testAuth.body.auth).toEqual(true);
     })
 
     test("authentication when signed out", async() => {
-        
+        const testAuth = await request.get("/auth/check-auth");
+        expect(testAuth.body.auth).toEqual(false);
     })
 })
