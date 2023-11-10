b8e6e2db743cc343817861afd6108d8f70b2815e
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-11-10 14:47:50 +0000

    Establish connection for TDD process

--- chat.test.js
+++ chat.test.js
@@ -1,13 +1,19 @@
 import supertest from "supertest";
 import app from "./server";
+import { io } from "socket.io-client";
 
 const request = supertest(app);
-
+beforeAll((done) => {
+    client = io.connect("http://localhost:3001");
+    client.on("connect", () => {
+        done();
+    })
+})
 describe("messaging", () => {
     test("send message", () => {
 
     })
     test ("receive message", () => {
-        
+
     })
 })
