a26a1df17384a0d3d90208b181f7b0d93ac8b56a
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-11-10 14:51:42 +0000

    Create two tests for sending and receiving messages

--- chat.test.js
+++ chat.test.js
@@ -8,12 +8,19 @@ beforeAll((done) => {
     client.on("connect", () => {
         done();
     })
+    secondClient = io.connect("http://localhost:3001");
 })
 describe("messaging", () => {
     test("send message", () => {
-
+        client.emit("send-message" ({text: "Hello", username: "Test"}));
+        client.once("correct-message", (data) => {
+            done();
+        })
     })
     test ("receive message", () => {
-
+        client.emit("send-message", ({text: "Hello", username: "Test"}));
+        secondClient.once("receive-message", (done) => {
+            done();
+        })
     })
 })
