ecaada9c942cffceff9f1c09aa7c26ea26e3acca
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-11-10 14:44:43 +0000

    Create files to begin TDD process for chat functionality

---
+++ chat.test.js
@@ -0,0 +1,13 @@
+import supertest from "supertest";
+import app from "./server";
+
+const request = supertest(app);
+
+describe("messaging", () => {
+    test("send message", () => {
+
+    })
+    test ("receive message", () => {
+        
+    })
+})
