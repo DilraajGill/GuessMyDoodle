f3636deeeb4017052fa7522bf9712c82d3b27b37
Dilraaj Gill <zkac282@live.rhul.ac.uk>
2023-11-19 17:04:15 +0000

    Add email field to form

--- front-end/src/RegisterPage.jsx
+++ front-end/src/RegisterPage.jsx
@@ -4,12 +4,14 @@ import axios from "axios";
 function RegisterPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
   async function submitForm(e) {
     e.preventDefault();
     var items = {
       username,
       password,
+      email,
     };
     console.log(items);
     try {
@@ -23,6 +25,13 @@ function RegisterPage() {
     <div>
       <h2>Register Page</h2>
       <form onSubmit={submitForm}>
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
         <input
           type="text"
           placeholder="Username"
