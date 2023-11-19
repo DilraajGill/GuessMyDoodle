d1f067bd7fc8d0808cf545a76deeb0015fc03a36
Dilraaj Gill <zkac282@live.rhul.ac.uk>
2023-11-19 22:15:25 +0000

    Ensure authentication when connecting to lobby

--- front-end/src/Lobby.jsx
+++ front-end/src/Lobby.jsx
@@ -1,15 +1,35 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import LineThickness from "./LineThickness";
 import ColourChooser from "./ColourChooser";
 import Canvas from "./Canvas";
 import ChatBox from "./ChatBox";
 import socket from "./SocketManager";
+import axios from "axios";
+import checkAuthentication from "./checkAuthentication";
 
 function Lobby() {
   const { lobbyId } = useParams();
   const [selectedColour, setSelectedColour] = React.useState("#00000");
   const [lineThickness, setLineThickness] = React.useState(2);
+  const navigation = useNavigate();
+
+  useEffect(() => {
+    async function ensureLogin() {
+      const response = await checkAuthentication({ axios });
+      if (response.auth) {
+        setSignedIn({
+          auth: true,
+          username: response.username,
+          // Temporary points
+          points: 0,
+        });
+      } else {
+        navigation("/login");
+      }
+    }
+    ensureLogin();
+  });
 
   return (
     <div>
