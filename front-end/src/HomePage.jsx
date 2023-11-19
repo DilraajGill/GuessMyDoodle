aedc26cf68e8467cfcf34f59973f47341f778540
Dilraaj Gill <zkac282@live.rhul.ac.uk>
2023-11-19 22:07:57 +0000

    Remove navigate from checkAuthentication

--- front-end/src/HomePage.jsx
+++ front-end/src/HomePage.jsx
@@ -1,33 +1,31 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import { authContext } from "./App";
 import { useNavigate } from "react-router-dom";
 import checkAuthentication from "./checkAuthentication";
 
-function HomePage(){
-    const [signedIn, setSignedIn] = React.useContext(authContext);
-    const navigation = useNavigate();
+function HomePage() {
+  const [signedIn, setSignedIn] = React.useContext(authContext);
+  const navigation = useNavigate();
 
-    useEffect(() => {
-        async function ensureLogin(){
-            const response = await checkAuthentication({axios, navigation});
-            if (response.auth){
-                setSignedIn({
-                    auth: true,
-                    username: response.username,
-                    // Temporary points assignment
-                    points: 0
-                })
-            } else {
-                navigation("/login");
-            }
-        }
-        ensureLogin();
-    })
+  useEffect(() => {
+    async function ensureLogin() {
+      const response = await checkAuthentication({ axios });
+      if (response.auth) {
+        setSignedIn({
+          auth: true,
+          username: response.username,
+          // Temporary points assignment
+          points: 0,
+        });
+      } else {
+        navigation("/login");
+      }
+    }
+    ensureLogin();
+  });
 
-    return(
-        <h1>Home Page</h1>
-    )
+  return <h1>Home Page</h1>;
 }
 
 export default HomePage;
--- front-end/src/checkAuthentication.jsx
+++ front-end/src/checkAuthentication.jsx
@@ -1,9 +1,9 @@
-async function checkAuthentication({axios, navigate}){
-    const response = await axios.get("/auth/check-auth");
+async function checkAuthentication({ axios }) {
+  const response = await axios.get("/auth/check-auth");
 
-    if (!response.data.auth){
-        return {auth: false};
-    }
-    return {auth: true};
+  if (!response.data.auth) {
+    return { auth: false };
+  }
+  return { auth: true };
 }
 export default checkAuthentication;
