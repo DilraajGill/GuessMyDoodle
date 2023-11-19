23a4d13a3fe6c05db12080b49c01ef5f33304930
Dilraaj Gill <zkac282@live.rhul.ac.uk>
2023-11-19 15:44:55 +0000

    Added code to pass test displaying page

--- front-end/src/Lobby.jsx
+++ front-end/src/Lobby.jsx
@@ -1,3 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-function Lobby() {}
+function Lobby() {
+  const { lobbyId } = useParams();
+
+  return (
+    <div>
+      <h1>Lobby ID: {lobbyId}</h1>
+    </div>
+  );
+}
+
+export default Lobby;
