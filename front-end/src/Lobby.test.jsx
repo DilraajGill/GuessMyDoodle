0176aaad1b1d455b875d4d116e75915266daaae2
Dilraaj Gill <zkac282@live.rhul.ac.uk>
2023-11-19 15:28:44 +0000

    Designed test to ensure lobby param displays on page

--- front-end/src/Lobby.test.jsx
+++ front-end/src/Lobby.test.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
+import Lobby from "./Lobby";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 describe("testing lobby home page", () => {
-  test("display lobby id at the top", () => {});
+  test("display lobby id at the top", () => {
+    const lobbyId = "abcdef";
+    render(
+      <MemoryRouter initialEntries={[`/lobby/${lobbyId}`]}>
+        <Routes>
+          <Route path="lobby/:lobbyId" element={<Lobby />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const lobbyIdText = screen.getByText(`Lobby ID: ${lobbyId}`);
+    expect(lobbyIdText).toBeInTheDocument();
+  });
 });
