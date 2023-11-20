fa87dd5ea952fbc37fb898d95455347f9664937d
Dilraaj Gill <zkac282@live.rhul.ac.uk>
2023-11-20 17:52:39 +0000

    Added code to pass tests

--- Game.js
+++ Game.js
@@ -1,10 +1,30 @@
 class Game {
-  constructor() {}
-  addPlayer() {}
-  removePlayer() {}
+  constructor() {
+    this.players = [];
+    this.host = null;
+    this.maxRounds = 0;
+    this.selectedTimer = 0;
+    this.timer = 60;
+  }
+  addPlayer(socket, username) {
+    this.players.push({ socket, username });
+    if (!this.host) {
+      this.host = socket;
+    }
+  }
+  removePlayer(socketId) {
+    this.players = this.players.filter(
+      (player) => player.socket.id !== socketId
+    );
+  }
   getPlayerList() {}
-  isHost() {}
-  start() {}
+  isHost(socket) {
+    return this.host === socket;
+  }
+  start(numberRounds, minutesOfDrawing) {
+    this.maxRounds = numberRounds;
+    this.selectedTimer = minutesOfDrawing * 60;
+  }
 }
 
 export default Game;
