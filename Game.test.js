fa2ad9fd075fe528f124521e83599b9d1e6e2571
Dilraaj Gill <zkac282@live.rhul.ac.uk>
2023-11-20 17:24:27 +0000

    Define tests to be made

---
+++ Game.test.js
@@ -0,0 +1,6 @@
+describe("Game class tests", () => {
+  test("add player to game", () => {});
+  test("remove player from game", () => {});
+  test("is the user host", () => {});
+  test("starting the game", () => {});
+});
