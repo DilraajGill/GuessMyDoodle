2907276eb5b69951f0ebadfe296249717d9055e6
Dilraaj Gill <zkac282@live.rhul.ac.uk>
2023-11-20 19:31:59 +0000

    Create tests to create lobby

---
+++ generate-lobby.test.js
@@ -0,0 +1,28 @@
+import supertest from "supertest";
+import app from "./server";
+
+const request = supertest(app);
+
+describe("request new lobby", () => {
+  let cookie;
+  test("valid request", async () => {
+    const testUser = {
+      username: "Test",
+      password: "testing123",
+    };
+    const response = await request
+      .post("/auth/login")
+      .send(testUser)
+      .set("Content-Type", "application/json");
+    cookie = response.headers["set-cookie"];
+    const lobbyResponse = await request
+      .post("/create-lobby")
+      .set("Cookie", cookie);
+    console.log(lobbyResponse);
+    expect(lobbyResponse.body.auth).toEqual(true);
+  });
+  test("invalid request", async () => {
+    const lobbyResponse = await request.post("/create-lobby");
+    expect(lobbyResponse.body.auth).toEqual(false);
+  });
+});
