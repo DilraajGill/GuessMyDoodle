16478c89b843a17525848d1c8372af9542919091
Dilraaj Gill <zkac282@live.rhul.ac.uk>
2023-11-20 21:13:20 +0000

    Add test to check invalid game

--- games.test.js
+++ games.test.js
@@ -7,52 +7,59 @@ const request = supertest(app);
 let client;
 let secondClient;
 beforeAll((done) => {
-    client = io.connect("http://localhost:3001");
-    secondClient = io.connect("http://localhost:3001");
-    client.on("connect", () => {
-        done();
-    })
-})
+  client = io.connect("http://localhost:3001");
+  secondClient = io.connect("http://localhost:3001");
+  client.on("connect", () => {
+    done();
+  });
+});
 describe("game tests", () => {
-    test("connection", async () => {
-        expect(client.connected).toBe(true);
-    })
+  test("connection", async () => {
+    expect(client.connected).toBe(true);
+  });
 
-    test("drawing client", (done) => {
-        client.emit("drawing", { x : 190, y : 290 });
+  test("drawing client", (done) => {
+    client.emit("drawing", { x: 190, y: 290 });
 
-        client.once("drawing", (data) => {
-            done();
-        })
-    })
+    client.once("drawing", (data) => {
+      done();
+    });
+  });
 
-    test("invalid drawing client", (done) => {
-        client.emit("drawing");
+  test("invalid drawing client", (done) => {
+    client.emit("drawing");
 
-        client.once("incorrectDrawing", () => {
-            done();
-        })
-    })
+    client.once("incorrectDrawing", () => {
+      done();
+    });
+  });
 
-    test("test begin drawing", (done) => {
-        client.emit("beginDrawing");
-        
-        client.once("beginDrawing", () => {
-            done();
-        })
-    })
+  test("test begin drawing", (done) => {
+    client.emit("beginDrawing");
 
-    test("first one drawing only", (done) => {
-        client.emit("test-drawing-allowed");
-        client.once("drawing-allowed", () => {
-            done();
-        })
-    })
+    client.once("beginDrawing", () => {
+      done();
+    });
+  });
 
-    test("second user cannot draw", (done) => {
-        secondClient.emit("test-drawing-allowed");
-        secondClient.once("drawing-not-allowed", () => {
-            done();
-        })
-    })
-})
+  test("first one drawing only", (done) => {
+    client.emit("test-drawing-allowed");
+    client.once("drawing-allowed", () => {
+      done();
+    });
+  });
+
+  test("second user cannot draw", (done) => {
+    secondClient.emit("test-drawing-allowed");
+    secondClient.once("drawing-not-allowed", () => {
+      done();
+    });
+  });
+
+  test("joining invalid game", (done) => {
+    client.emit("join-lobby", { lobbyId: "ABCDE", username: "Test" });
+    client.once("invalid-game", () => {
+      done();
+    });
+  });
+});
