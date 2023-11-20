21f432a0c7e1b8e92766ee5d13618bad204be9ba
Dilraaj Gill <zkac282@live.rhul.ac.uk>
2023-11-20 17:45:56 +0000

    Created basic tests for adding & removing players

--- Game.test.js
+++ Game.test.js
@@ -1,6 +1,30 @@
+import Game from "./Game";
+const gameSession = new Game();
+const mockSocket = { emit: jest.fn(), id: "first" };
+
 describe("Game class tests", () => {
-  test("add player to game", () => {});
-  test("remove player from game", () => {});
-  test("is the user host", () => {});
-  test("starting the game", () => {});
+  test("add player to game", () => {
+    gameSession.addPlayer(mockSocket, "test");
+    expect(gameSession.players).toHaveLength(1);
+    expect(gameSession.host).toBe(mockSocket);
+  });
+  test("remove player from game", () => {
+    const secondMock = { emit: jest.fn(), id: "second" };
+    gameSession.addPlayer(secondMock, "test2");
+    gameSession.removePlayer(secondMock.id);
+    expect(gameSession.players).toHaveLength(1);
+  });
+  test("starting the game", () => {
+    gameSession.start(3, 2);
+    expect(gameSession.maxRounds).toBe(3);
+    expect(gameSession.selectedTimer).toBe(120);
+  });
+  test("checking who is host", () => {
+    console.log(gameSession.host);
+    expect(gameSession.isHost(mockSocket)).toBe(true);
+  });
+  test("checking if not host", () => {
+    const notHost = { id: "second" };
+    expect(gameSession.isHost(notHost)).toBe(false);
+  });
 });
