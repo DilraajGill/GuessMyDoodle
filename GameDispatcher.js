4d2af6fe932f0fb8f96e7ed59cbee2d1a1e199bf
Dilraaj Gill <zkac282@live.rhul.ac.uk>
2023-11-21 17:10:40 +0000

    Move server interaction with game to dispatcher

--- GameDispatcher.js
+++ GameDispatcher.js
@@ -1,4 +1,4 @@
-import Game from "./Game";
+import Game from "./Game.js";
 
 class GameDispatcher {
   constructor(io) {
@@ -21,6 +21,7 @@ class GameDispatcher {
       generatedID = this.generateLobbyID();
     }
     this.games[generatedID] = new Game(generatedID, this.io);
+    console.log(this.games);
     return generatedID;
   }
   generateLobbyID() {
@@ -33,14 +34,36 @@ class GameDispatcher {
     }
     return output;
   }
-  joinGame(lobbyId, socket) {
+  joinGame(lobbyId, socket, username) {
     if (this.checkExists(lobbyId)) {
+      socket.username = username;
+      socket.lobbyId = lobbyId;
+      socket.points = 0;
       socket.join(lobbyId);
-      this.games[lobbyId].addPlayer(socket, socket.username);
+      this.games[lobbyId].addPlayer(socket, username);
+      console.log(`Added ${username} to the lobby`);
+    } else {
+      socket.emit("invalid-game");
     }
   }
   messageGame(lobbyId, text, username) {
-    this.io.to(lobbyId).emit("receive-message", { text, username });
+    if (this.checkExists(lobbyId)) {
+      this.io.to(lobbyId).emit("receive-message", { text, username });
+    }
+  }
+  checkHost(lobbyId, socket) {
+    return this.games[lobbyId].host === socket;
+  }
+  addDrawing(lobbyId, socket, data) {
+    if (!this.checkExists(lobbyId)) {
+      socket.emit("incorrectDrawing");
+    } else if (this.checkHost(lobbyId, socket)) {
+      this.io.to(lobbyId).emit("drawing", data);
+      this.games[lobbyId].addDrawing(data);
+    }
+  }
+  beganDrawing(lobbyId) {
+    this.io.to(lobbyId).emit("beginDrawing");
   }
 }
 
