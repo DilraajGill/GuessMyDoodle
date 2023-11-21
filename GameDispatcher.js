45a82ebe975b2d4130128bb5806688aa60ff2520
Dilraaj Gill <zkac282@live.rhul.ac.uk>
2023-11-21 16:38:11 +0000

    Add code to pass tests

--- GameDispatcher.js
+++ GameDispatcher.js
@@ -1,5 +1,38 @@
+import Game from "./Game";
+
 class GameDispatcher {
-  constructor() {
+  constructor(io) {
     this.games = {};
+    this.io = io;
+  }
+  checkExists(lobbyId) {
+    return !!this.games[lobbyId];
+  }
+  remove(lobbyId) {
+    if (this.checkExists(lobbyId)) {
+      delete this.games[lobbyId];
+      return true;
+    }
+    return false;
+  }
+  create() {
+    const generatedID = this.generateLobbyID();
+    while (this.checkExists(generatedID)) {
+      generatedID = this.generateLobbyID();
+    }
+    this.games[generatedID] = new Game(generatedID, this.io);
+    return generatedID;
+  }
+  generateLobbyID() {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let output = "";
+    for (let x = 0; x < 5; x++) {
+      output += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    return output;
   }
 }
+
+export default GameDispatcher;
