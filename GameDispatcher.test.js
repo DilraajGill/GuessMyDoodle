c7aa25251e4fdfe22570fa4c4c38fe6d017465bd
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-11-26 23:35:58 +0000

    Updated test code to handle drawing functions

--- GameDispatcher.test.js
+++ GameDispatcher.test.js
@@ -1,3 +1,4 @@
+import Game from "./Game";
 import GameDispatcher from "./GameDispatcher";
 import { Server } from "socket.io";
 
@@ -14,6 +15,7 @@ jest.mock("socket.io", () => {
 });
 const mockIo = new Server();
 const Games = new GameDispatcher(mockIo);
+const mockSocket = { emit: jest.fn(), id: "first", join: jest.fn() };
 
 describe("Testing Game dispatcher", () => {
   let addedLobby;
@@ -26,9 +28,34 @@ describe("Testing Game dispatcher", () => {
     const outcome = Games.checkExists(addedLobby);
     expect(outcome).toBe(true);
   });
+  test("handle joining game", () => {
+    Games.joinGame(addedLobby, mockSocket, "Test");
+    expect(mockIo.to).toHaveBeenCalledWith(addedLobby);
+  });
+  test("send message to dispatcher", () => {
+    Games.messageGame(addedLobby, "Hello", "Test");
+    expect(mockSocket.join).toHaveBeenCalled();
+    expect(mockIo.to).toHaveBeenCalledWith(addedLobby);
+  });
+  test("add drawing", () => {
+    Games.addDrawing(addedLobby, mockSocket, { x: 190, y: 290 });
+    expect(mockIo.to).toHaveBeenCalledWith(addedLobby);
+  });
   test("remove game from dispatcher", () => {
     const outcome = Games.remove(addedLobby);
     expect(outcome).toBe(true);
+  });
+  test("update minutes", () => {
+    Games.updateMinutes(mockSocket, 2);
+    expect(mockIo.to).toHaveBeenCalledWith(addedLobby);
+  });
+  test("update max rounds", () => {
+    Games.updateRounds(mockSocket, 2);
+    expect(mockIo.to).toHaveBeenCalledWith(addedLobby);
+  });
+  test("remove added player", () => {
+    Games.removePlayer(mockSocket);
+    expect(mockIo.to).toHaveBeenCalledWith(addedLobby);
   });
   test("remove game that does not exist", () => {
     const outcome = Games.remove("AASDASD");
