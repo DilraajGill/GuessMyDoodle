3a6d8636790902f00fc1e88571277a65ce0fef25
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-12-07 13:48:30 +0000

    Create code to pass test

--- front-end/src/CurrentlyDrawing.jsx
+++ front-end/src/CurrentlyDrawing.jsx
@@ -1,2 +1,10 @@
-function CurrentlyDrawing() {}
+import React from "react";
+
+function CurrentlyDrawing({ username }) {
+  return (
+    <div>
+      <h1>{username} is currently drawing!</h1>
+    </div>
+  );
+}
 export default CurrentlyDrawing;
