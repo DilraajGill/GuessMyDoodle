603ab0e93d56727189afb6838c8f943d6a40bb18
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-12-07 14:59:19 +0000

    Refactored test code to work

--- front-end/src/Lobby.test.jsx
+++ front-end/src/Lobby.test.jsx
@@ -1,18 +1,31 @@
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Lobby from "./Lobby";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
+import { authContext } from "./App";
 
+jest.mock("axios", () => ({
+  get: jest
+    .fn()
+    .mockResolvedValue({ data: { auth: true, username: "Test", points: 0 } }),
+}));
+
+const mockAuthContextValue = [
+  { auth: true, username: "Test", points: 0 },
+  jest.fn(),
+];
 describe("testing lobby home page", () => {
   // test to ensure lobby id is displayed on screen
   test("display lobby id at the top", () => {
     const lobbyId = "abcdef";
     render(
-      <MemoryRouter initialEntries={[`/lobby/${lobbyId}`]}>
-        <Routes>
-          <Route path="lobby/:lobbyId" element={<Lobby />} />
-        </Routes>
-      </MemoryRouter>
+      <authContext.Provider value={mockAuthContextValue}>
+        <MemoryRouter initialEntries={[`/lobby/${lobbyId}`]}>
+          <Routes>
+            <Route path="lobby/:lobbyId" element={<Lobby />} />
+          </Routes>
+        </MemoryRouter>
+      </authContext.Provider>
     );
 
     const lobbyIdText = screen.getByText(`Lobby ID: ${lobbyId}`);
