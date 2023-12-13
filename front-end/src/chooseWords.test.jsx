9cd7fe83e3cbd518418e6627262b45ba0ecd8431
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-12-13 16:46:02 +0000

    Define tests for front-end component

--- front-end/src/chooseWords.test.jsx
+++ front-end/src/chooseWords.test.jsx
@@ -1,8 +1,20 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import chooseWords from "./chooseWords";
 
 describe("display selection of words", () => {
-  test("display words", () => {});
-  test("clicking button calls function", () => {});
+  const listOfWords = ["Dilraaj", "Was", "Here"];
+  const mockFunction = jest.fn();
+  test("display words", () => {
+    render(<chooseWords list={listOfWords} click={mockFunction} />);
+    expect(screen.getByText(listOfWords[0])).toBeInTheDocument();
+    expect(screen.getByText(listOfWords[1])).toBeInTheDocument();
+    expect(screen.getByText(listOfWords[2])).toBeInTheDocument();
+  });
+  test("clicking button calls function", () => {
+    render(<chooseWords list={listOfWords} click={mockFunction} />);
+    const firstButton = screen.getByLabelText("Dilraaj");
+    fireEvent.click(firstButton);
+    expect(mockFunction).toHaveBeenCalled();
+  });
 });
