f2f5e83fc7bf89a15b634e8a6add4cf3940ea763
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-12-13 17:28:29 +0000

    Create code to pass tests

--- front-end/src/chooseWords.jsx
+++ front-end/src/chooseWords.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
-function chooseWords() {}
+function ChooseWords({ list, click }) {
+  console.log(list);
+  return (
+    <div>
+      <h3>Select a word:</h3>
+      {list.map((word) => (
+        <button key={word} onClick={() => click(word)}>
+          {word}
+        </button>
+      ))}
+    </div>
+  );
+}
 
-export default chooseWords;
+export default ChooseWords;
