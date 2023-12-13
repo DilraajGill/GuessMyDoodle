093a4d63d27eb2f440bd4e979a4558dae9fb84d0
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-12-13 16:35:36 +0000

    Defined tests for displaying words

---
+++ front-end/src/chooseWords.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+function chooseWords() {}
+
+export default chooseWords;
---
+++ front-end/src/chooseWords.test.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import chooseWords from "./chooseWords";
+
+describe("display selection of words", () => {
+  test("display words", () => {});
+  test("clicking button calls function", () => {});
+});
