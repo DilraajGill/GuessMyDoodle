0f8b5bf58308fe89e4cf7add2edcdc53ad4e87b3
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-12-24 14:07:00 +0000

    Create code to pass test

--- front-end/src/PlayerCard.jsx
+++ front-end/src/PlayerCard.jsx
@@ -1,3 +1,10 @@
 import React from "react";
-function PlayerCard() {}
+function PlayerCard({ player, points, colour }) {
+  return (
+    <div>
+      <h4>{player}</h4>
+      <h6>{points}</h6>
+    </div>
+  );
+}
 export default PlayerCard;
