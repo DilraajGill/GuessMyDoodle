dbcca30a41b7e2d7d52d4bd19ab989ed2f7c5c38
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-12-24 05:19:03 +0000

    Create function for retrieving lobbies

--- front-end/src/HomePage.jsx
+++ front-end/src/HomePage.jsx
@@ -10,6 +10,7 @@ import checkAuthentication from "./checkAuthentication";
 function HomePage() {
   // Access context from app and define navigation object
   const [signedIn, setSignedIn] = React.useContext(authContext);
+  const [lobbies, setLobbies] = React.useState([]);
   const navigation = useNavigate();
 
   // Ensure the user is signed in
@@ -30,6 +31,10 @@ function HomePage() {
         // If not signed in, navigate them to the /login page
         navigation("/login");
       }
+    }
+    async function loadLobbies() {
+      const response = await axios.get("/get-public");
+      setLobbies(response);
     }
     ensureLogin();
   });
@@ -53,6 +58,7 @@ function HomePage() {
       <h2>Welcome {signedIn.username}</h2>
       <h2>Your Points: {signedIn.points}</h2>
       <button onClick={createLobby}>Create Lobby!</button>
+      <div></div>
     </div>
   );
 }
