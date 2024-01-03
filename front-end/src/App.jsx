3bfef51afbf2a298889c7b9d6f5373bf00dec82f
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2024-01-03 18:46:29 +0000

    Add front-end route for completing profile

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from "./HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Canvas from "./Canvas";
 import Lobby from "./Lobby";
+import CompleteProfile from "./CompleteProfile";
 
 // Create context to store authentication
 /**
@@ -29,6 +30,7 @@ function App() {
           <Route path="/home" element={<HomePage />} />
           <Route path="/lobby/:lobbyId" element={<Lobby />} />
           <Route path="canvas" element={<Canvas />} />
+          <Route path="complete-profile" element={<CompleteProfile />} />
         </Routes>
       </BrowserRouter>
     </authContext.Provider>
