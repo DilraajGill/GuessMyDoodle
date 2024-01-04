2c00c07cdc896239c5b95c4d9951540f8efe6c0c
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2024-01-04 16:05:05 +0000

    Add route to back-end for purchasing

--- front-end/src/Store.jsx
+++ front-end/src/Store.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import axios from "axios";
 function Store() {
   async function purchaseFillTool() {
-    console.log("Running");
+    try {
+      const response = await axios.post("/store/buy/fill-tool");
+      console.log(response.data.success);
+    } catch (error) {
+      console.log("Error purchasing fill tool");
+    }
   }
   return (
     <div>
