1d8f0c9eb039b62a1c3dfb33be71a9f8c0f3f99c
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2024-01-04 15:56:10 +0000

    Refactor code for displaying fill tool

--- front-end/src/Store.jsx
+++ front-end/src/Store.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import axios from "axios";
 function Store() {
+  async function purchaseFillTool() {
+    console.log("Running");
+  }
   return (
     <div>
       <h1>Welcome To The Store!</h1>
+      <div>
+        <h2>Fill Tool</h2>
+        <button onClick={purchaseFillTool}>Buy Fill Tool</button>
+      </div>
     </div>
   );
 }
