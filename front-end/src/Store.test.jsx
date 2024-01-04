90e0485a288394c34a5a052e2e5b4454af428233
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2024-01-04 14:48:40 +0000

    Define first test

---
+++ front-end/src/Store.test.jsx
@@ -0,0 +1,7 @@
+import Store from "./Store";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+describe("test store display", () => {
+  test("display welcome message", () => {});
+});
