44c2cdee2a5d302781ea695a9cb45d426453402d
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2024-01-04 14:49:50 +0000

    Create code for first test

--- front-end/src/Store.test.jsx
+++ front-end/src/Store.test.jsx
@@ -3,5 +3,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 
 describe("test store display", () => {
-  test("display welcome message", () => {});
+  test("display welcome message", () => {
+    render(<Store />);
+    expect(screen.getByText("Welcome To The Store!")).toBeInTheDocument();
+  });
 });
