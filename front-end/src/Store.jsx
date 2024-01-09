cc55f186659814637f21bdb246a798e43faf33c2
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2024-01-09 19:03:06 +0000

    Ensure only authorised people are allowed to visit the store

--- front-end/src/Store.jsx
+++ front-end/src/Store.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
+import checkAuthentication from "./checkAuthentication";
+import { authContext } from "./App";
+import { useNavigate } from "react-router-dom";
+
 function Store() {
+  const [signedIn, setSignedIn] = React.useContext(authContext);
+  const navigation = useNavigate();
+  useEffect(() => {
+    async function ensureLogin() {
+      const response = await checkAuthentication({ axios });
+      if (response.auth) {
+        setSignedIn({
+          auth: true,
+          username: response.username,
+          points: response.points,
+          tools: response.tools,
+        });
+      } else {
+        // If not signed in, navigate them to the /login page
+        navigation("/login");
+      }
+    }
+    ensureLogin();
+  }, []);
   async function purchaseFillTool() {
     try {
       const response = await axios.post("/store/buy/fill-tool");
