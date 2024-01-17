1de195955f50336eeed60c29148c9fc97134c7ac
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2024-01-17 15:15:15 +0000

    Re-structure combined component to be aligned in the middle

--- front-end/src/LoginAndRegister.jsx
+++ front-end/src/LoginAndRegister.jsx
@@ -2,28 +2,29 @@ import React from "react";
 import { Tabs, Tab, Container, Row, Col, Card } from "react-bootstrap";
 import LoginPage from "./LoginPage";
 import RegisterPage from "./RegisterPage";
-
+import "./LoginAndRegister.css";
 function LoginAndRegister() {
   const [state, setState] = React.useState("login");
 
   return (
-    <Container>
-      <Row>
+    <Container fluid className="d-flex vh-100">
+      <Row className="m-auto align-self-center">
         <Col>
           <Card>
-            <Tabs
-              id="tab-state"
-              activeKey={state}
-              onSelect={(e) => setState(e)}
-              className="nav-pills nav-justified"
-            >
-              <Tab eventKey="login" title="Login">
-                <LoginPage />
-              </Tab>
-              <Tab eventKey="register" title="Register">
-                <RegisterPage />
-              </Tab>
-            </Tabs>
+            <Card.Header>
+              <Tabs
+                id="tab-state"
+                activeKey={state}
+                onSelect={(e) => setState(e)}
+                className="nav-pills justify-content-center"
+              >
+                <Tab eventKey="login" title="Login"></Tab>
+                <Tab eventKey="register" title="Register"></Tab>
+              </Tabs>
+            </Card.Header>
+            <Card.Body>
+              {state === "login" ? <LoginPage /> : <RegisterPage />}
+            </Card.Body>
           </Card>
         </Col>
       </Row>
