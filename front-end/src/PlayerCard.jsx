f779dce2a41e34108ec3d32a5d1f6a0fbb69cad9
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2024-01-29 21:58:37 +0000

    Display icon next to player if they are drawing

--- front-end/src/PlayerCard.jsx
+++ front-end/src/PlayerCard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-function PlayerCard({ player, points, colour }) {
+function PlayerCard({ player, points, colour, drawing }) {
   return (
     <Card>
       <Card.Body>
-        <Card.Title>{player}</Card.Title>
+        <Card.Title>
+          {player} {drawing && <i class="bi bi-pencil-fill"></i>}
+        </Card.Title>
         <Card.Text>Points: {points}</Card.Text>
       </Card.Body>
     </Card>
