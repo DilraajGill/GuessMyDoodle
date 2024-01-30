19ba2cddcd2fb67e449d131b4f89daaed1b54bf2
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2024-01-30 23:02:55 +0000

    Add margin to adjust the structure of group

--- front-end/src/PlayerCard.jsx
+++ front-end/src/PlayerCard.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { Card } from "react-bootstrap";
 function PlayerCard({ player, points, colour, drawing, host }) {
   return (
-    <Card>
-      <Card.Body>
-        <Card.Title>
-          {player}
-          {host && <i className="bi bi-star-fill"></i>}
-          {drawing && <i class="bi bi-pencil-fill"></i>}
-        </Card.Title>
-        <Card.Text>Points: {points}</Card.Text>
-      </Card.Body>
-    </Card>
+    <>
+      <Card className="mb-1">
+        <Card.Body>
+          <Card.Title>
+            {player}
+            {host && <i className="bi bi-star-fill"></i>}
+            {drawing && <i class="bi bi-pencil-fill"></i>}
+          </Card.Title>
+          <Card.Text>Points: {points}</Card.Text>
+        </Card.Body>
+      </Card>
+    </>
   );
 }
 export default PlayerCard;
