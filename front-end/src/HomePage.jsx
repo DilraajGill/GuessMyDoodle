b573506b05a32503dab4f19cd0b70b5e80312cc8
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2024-02-05 22:18:56 +0000

    Finish adjusting new components of the Home Page

--- front-end/src/HomePage.jsx
+++ front-end/src/HomePage.jsx
@@ -63,51 +63,66 @@ function HomePage() {
   }
 
   return (
-    <Container className="text-center home-page">
-      <div className="top-bar d-flex justify-content-between align-items-center">
-        <div className="toolbar">
-          <Button
-            className="me-3"
-            variant="primary"
-            onClick={() => navigation("/store")}
-          >
-            Go To Store!
-          </Button>
-          <Button
-            variant="primary"
-            onClick={() => loadLobbies()}
-            className="me-3"
-          >
-            <i class="bi bi-arrow-clockwise"></i>
-          </Button>
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <Container className="text-center home-page">
+        <div className="top-bar align-items-center">
+          <Row>
+            <Col md={4}>
+              <div className="toolbar">
+                <Button
+                  className="me-3"
+                  variant="primary"
+                  onClick={() => navigation("/store")}
+                >
+                  Go To Store!
+                </Button>
+                <Button
+                  variant="primary"
+                  onClick={() => loadLobbies()}
+                  className="me-3"
+                >
+                  <i class="bi bi-arrow-clockwise"></i>
+                </Button>
+              </div>
+            </Col>
+            <Col md={4}>
+              <h2>Home</h2>
+            </Col>
+            <Col md={4}>
+              <div className="points-container text-right mr-3">
+                <div className="points-label">Points</div>
+                <div className="points-value">
+                  <strong>{signedIn.points}</strong>
+                </div>
+              </div>
+            </Col>
+          </Row>
         </div>
 
-        <div className="points-container text-right mr-3">
-          <div className="points-label">Points</div>
-          <div className="points-value">
-            <strong>{signedIn.points.toLocaleString()}</strong>
-          </div>
+        <div className="room-container my-3 p-3">
+          {lobbies.length > 0 ? (
+            <Row xs={1} md={3} lg={4}>
+              {lobbies.map((lobby) => (
+                <Col>
+                  <LobbyCard
+                    key={lobby.id}
+                    lobby={lobby}
+                    onClick={() => handleLobbyClick(lobby.id)}
+                  />
+                </Col>
+              ))}
+            </Row>
+          ) : (
+            <div className="no-lobbies">No lobbies available!</div>
+          )}
         </div>
-      </div>
-      <h2>Welcome {signedIn.username}</h2>
-      <div className="toolbar">
-        <Button variant="primary" onClick={createLobby} className="me-3">
-          Create Lobby!
-        </Button>
-      </div>
-      <br />
-      <Row xs={1} md={3} lg={5}>
-        {lobbies.map((lobby) => (
-          <Col>
-            <LobbyCard
-              key={lobby.id}
-              lobby={lobby}
-              onClick={() => handleLobbyClick(lobby.id)}
-            />
-          </Col>
-        ))}
-      </Row>
-    </Container>
+        <div className="toolbar">
+          <Button variant="primary" onClick={createLobby} className="me-3">
+            Create Lobby!
+          </Button>
+        </div>
+      </Container>
+    </div>
   );
 }
 
