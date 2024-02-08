34b560929958ce953ac995be46bcf185bb8553f3
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2024-02-08 13:49:19 +0000

    Remove Toast to provide a consistent design scheme

--- front-end/src/CopyToClipboard.jsx
+++ front-end/src/CopyToClipboard.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Card, Toast } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 function CopyToClipboard({ lobbyId }) {
-  const [showMessage, setShowMessage] = React.useState(false);
+  const [copied, setCopied] = React.useState(false);
 
   async function copy() {
     try {
       const lobby = `localhost:3000/lobby/${lobbyId}`;
       await navigator.clipboard.writeText(lobby);
-      setShowMessage(true);
-      setTimeout(() => setShowMessage(false), 4000);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
     } catch (err) {
       console.log("Failed to copy");
     }
@@ -17,25 +17,19 @@ function CopyToClipboard({ lobbyId }) {
   return (
     <>
       <Card
-        className="mt-2 p-2 text-center card-clipboard"
+        className={`mt-2 p-2 text-center card-clipboard ${
+          copied ? "copied" : ""
+        }`}
         onClick={copy}
         role="clipboard-button"
       >
         <Card.Title>
           Click to copy URL to send to your friends! Lobby ID: {lobbyId}
         </Card.Title>
+        {copied && (
+          <div className="clipboard-feedback">Copied To Clipboard!</div>
+        )}
       </Card>
-      <Toast
-        show={showMessage}
-        onClose={() => setShowMessage(false)}
-        delay={4000}
-        autohide
-      >
-        <Toast.Header>
-          <strong>Success!</strong>
-        </Toast.Header>
-        <Toast.Body>Link has been copied to clipboard!</Toast.Body>
-      </Toast>
     </>
   );
 }
