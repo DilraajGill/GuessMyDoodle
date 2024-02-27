328047207ebdb73ee192575fb15ee9bcc1716c2c
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2024-02-27 10:23:39 +0000

    Structure how the new profile picture selector should appear

--- front-end/src/PictureSelector.jsx
+++ front-end/src/PictureSelector.jsx
@@ -1,5 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
 import axios from "axios";
+import "./HomePage.css";
+import "./PictureSelector.css";
 
 function PictureSelector({ currentPicture, availablePictures }) {
   const [selectedPicture, setSelectedPicture] = React.useState(null);
@@ -8,20 +11,32 @@ function PictureSelector({ currentPicture, availablePictures }) {
     // POST to the server the new profile picture to use
   }
   return (
-    <div>
-      <img
-        src={currentPicture}
-        data-testId="current"
-        className="current-picture"
-      />
-      {availablePictures.map((pic) => (
-        <img
-          src={`../${pic}`}
-          key={pic}
-          data-testId="available"
-          className="available-picture"
-        ></img>
-      ))}
+    <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+      <Container className="text-center">
+        <div className="home-page">
+          <Row>
+            <Col md={6}>
+              <img
+                src={currentPicture}
+                data-testId="current"
+                className="current-picture"
+                alt="Current Icon"
+              />
+            </Col>
+            <Col md={6}>
+              {availablePictures.map((pic) => (
+                <img
+                  src={`../${pic}`}
+                  key={pic}
+                  data-testId="available"
+                  className="available-picture"
+                  alt="Available Icon"
+                ></img>
+              ))}
+            </Col>
+          </Row>
+        </div>
+      </Container>
     </div>
   );
 }
