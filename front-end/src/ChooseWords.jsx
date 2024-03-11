787f5951c4b1b930fff16d637b1a828b60af532a
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2024-03-11 10:47:07 +0000

    Utilise React-Bootstrap buttons over standard HTML

--- front-end/src/ChooseWords.jsx
+++ front-end/src/ChooseWords.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 function ChooseWords({ list, click }) {
   return (
     <div>
       <h3>Select a word:</h3>
       {list.map((word) => (
-        <button key={word} onClick={() => click(word)}>
+        <Button className="m-2" key={word} onClick={() => click(word)}>
           {word}
-        </button>
+        </Button>
       ))}
     </div>
   );
