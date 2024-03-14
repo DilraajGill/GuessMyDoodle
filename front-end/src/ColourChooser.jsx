17afa34bacb1a57068b59b3bf5bca3a9a02470df
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2024-03-14 14:34:06 +0000

    Display dark colours alongside light colours

--- front-end/src/ColourChooser.jsx
+++ front-end/src/ColourChooser.jsx
@@ -8,19 +8,35 @@ import React from "react";
 function ColourChooser({ toCanvas }) {
   // Create state to store the chosen colour
   const [selectedColour, setSelectedColour] = React.useState("#000000");
-  const availableColours = [
+  const availableLightColours = [
     "#FFFFFF",
-    "#C1C1C1",
-    "#EF130B",
-    "#FF7100",
-    "#FFE400",
-    "#00CC00",
-    "#00B2FF",
-    "#231FD3",
-    "#A300BA",
-    "#D37CAA",
-    "#A0522D",
+    "#A5A3A3",
+    "#CF0000",
+    "#CF5300",
+    "#CEC900",
+    "#00C300",
+    "#00C374",
+    "#00A3CF",
+    "#0B09CF",
+    "#9E00B2",
+    "#CE478F",
+    "#CF8A68",
+    "#953910",
+  ];
+  const availableDarkColours = [
     "#000000",
+    "#3A3A3A",
+    "#6D0000",
+    "#BC1B00",
+    "#C98300",
+    "#003A05",
+    "#016848",
+    "#014393",
+    "#00005E",
+    "#4C005F",
+    "#7A193F",
+    "#B8572B",
+    "#571900",
   ];
   // Create method to update state and communicate this change to lobby
   /**
@@ -34,18 +50,34 @@ function ColourChooser({ toCanvas }) {
   }
   return (
     <div className="colour-picker">
-      {availableColours.map((colour) => (
-        <div
-          key={colour}
-          className={`colour-item ${
-            colour === selectedColour ? "selected" : ""
-          }`}
-          style={{ backgroundColor: colour }}
-          onClick={() => changeColour(colour)}
-        >
-          <div className="colour-inner" style={{ backgroundColor: colour }} />
-        </div>
-      ))}
+      <div className="light-colours">
+        {availableLightColours.map((colour) => (
+          <div
+            key={colour}
+            className={`colour-item light ${
+              colour === selectedColour ? "selected" : ""
+            }`}
+            style={{ backgroundColor: colour }}
+            onClick={() => changeColour(colour)}
+          >
+            <div className="colour-inner" style={{ backgroundColor: colour }} />
+          </div>
+        ))}
+      </div>
+      <div className="dark-colours">
+        {availableDarkColours.map((colour) => (
+          <div
+            key={colour}
+            className={`colour-item dark ${
+              colour === selectedColour ? "selected" : ""
+            }`}
+            style={{ backgroundColor: colour }}
+            onClick={() => changeColour(colour)}
+          >
+            <div className="colour-inner" style={{ backgroundColor: colour }} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
