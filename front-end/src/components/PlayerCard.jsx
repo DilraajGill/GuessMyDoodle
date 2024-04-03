880231fe886dac21d8892aab6cfa24ecd651525f
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2024-04-03 16:01:48 +0100

    Refactor PlayerCard to scale images better

--- front-end/src/components/PlayerCard.jsx
+++ front-end/src/components/PlayerCard.jsx
@@ -1,45 +1,39 @@
 import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
+
 function PlayerCard({ player, points, picture, drawing, host, kick }) {
   const address = `../${picture}`;
 
-  function kickUser() {
-    kick(player);
-  }
-
   return (
-    <>
-      <Card style={{ cursor: "pointer" }} onClick={kickUser}>
-        <Row>
-          <Col md={3}>
-            <Card.Img variant="top" src={address} style={{ height: "100%" }} />
-          </Col>
-          <Col md={9}>
-            <Card.Body>
-              <Card.Title>
-                {player}
-                <div className="ms-1" style={{ display: "inline-block" }}>
-                  {host && (
-                    <i className="bi bi-star-fill" data-testid="host-icon"></i>
-                  )}
-                </div>
-                <div className="ms-1" style={{ display: "inline-block" }}>
-                  {drawing && (
-                    <i
-                      className="bi bi-pencil-fill"
-                      data-testid="drawing-icon"
-                    ></i>
-                  )}
-                </div>
-              </Card.Title>
-              <Card.Text style={{ display: "block" }}>
-                Points: {points}
-              </Card.Text>
-            </Card.Body>
-          </Col>
-        </Row>
-      </Card>
-    </>
+    <Card
+      className="mb-2"
+      style={{ cursor: "pointer" }}
+      onClick={() => kick(player)}
+    >
+      <Row noGutters className="align-items-center">
+        <Col xs={3} className="d-flex justify-content-center">
+          <Card.Img variant="top" src={address} className="img-fluid" />
+        </Col>
+        <Col xs={9} className="p-2">
+          <Card.Body className="p-0">
+            <Card.Title className="mb-0">
+              {player}
+              {host && (
+                <i className="bi bi-star-fill ms-2" data-testid="host-icon"></i>
+              )}
+              {drawing && (
+                <i
+                  className="bi bi-pencil-fill ms-2"
+                  data-testid="drawing-icon"
+                ></i>
+              )}
+            </Card.Title>
+            <Card.Text>Points: {points}</Card.Text>
+          </Card.Body>
+        </Col>
+      </Row>
+    </Card>
   );
 }
+
 export default PlayerCard;
