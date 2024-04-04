81fd439cacac76753b6151a378592089e98719ea
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2024-04-04 11:32:41 +0100

    Fix issue where item was not aligned centrally

--- front-end/src/features/Lobby/ChooseWords.jsx
+++ front-end/src/features/Lobby/ChooseWords.jsx
@@ -3,13 +3,15 @@ import { Button } from "react-bootstrap";
 
 function ChooseWords({ list, click }) {
   return (
-    <div>
+    <div className="d-flex flex-column align-items-center justify-content-center">
       <h3>Select a word:</h3>
-      {list.map((word) => (
-        <Button className="m-2" key={word} onClick={() => click(word)}>
-          {word}
-        </Button>
-      ))}
+      <div className="d-flex">
+        {list.map((word) => (
+          <Button className="m-2" key={word} onClick={() => click(word)}>
+            {word}
+          </Button>
+        ))}
+      </div>
     </div>
   );
 }
